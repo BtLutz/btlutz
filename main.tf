42e5a47cf1e73cb209b283768eade36bab7dd479
Brian Lutz <brian.lutz9@icloud.com>
2025-01-20 01:07:08 -0500

    Add an ingress rule in main.tf
    Required as a prerequisite in the guide I'm following: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/getting-started-fargate.html#first-run-linux-prereqs

--- main.tf
+++ main.tf
@@ -31,7 +31,12 @@ resource "aws_ecs_cluster" "btlutz" {
 }
 
 resource "aws_security_group" "web-sg" {
-  name    = "btlutz-sg"
-  ingress = []
-  egress  = []
+  name = "btlutz-sg"
+  ingress = {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress = []
 }
