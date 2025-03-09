e0ba3d27c65de4b9fe625a779cd649d0d640b6fa
Brian Lutz <brian.lutz9@icloud.com>
2025-03-09 00:25:18 -0500

    Add an egress block in main.tf

--- main.tf
+++ main.tf
@@ -84,7 +84,12 @@ resource "aws_security_group" "btlutz" {
     self        = "false"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress = []
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_launch_template" "btlutz" {
