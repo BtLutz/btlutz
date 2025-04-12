c2e624468c1e72b16508c34e259b8fe0610e5e03
Brian Lutz <brian.lutz9@icloud.com>
2025-04-12 17:28:22 -0400

    Add `aws_route53_record` in main.tf

--- main.tf
+++ main.tf
@@ -196,3 +196,11 @@ resource "aws_ecs_service" "btlutz" {
 resource "aws_route53_zone" "primary" {
   name = "btlutz.com"
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name = "www.btlutz.com"
+  type = "A"
+  ttl = 300
+  records = [aws_alb.btlutz.dns_name]
+}
