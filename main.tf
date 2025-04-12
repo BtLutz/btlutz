365b351d30f2b725a453c92d194c5b456af19d3f
Brian Lutz <brian.lutz9@icloud.com>
2025-04-12 13:54:47 -0400

    Add `aws_default_vpc` and `aws_default_subnet`

--- main.tf
+++ main.tf
@@ -22,6 +22,20 @@ provider "aws" {
   region = local.region
 }
 
+resource "aws_default_vpc" "default_vpc" {}
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-1c"
+}
+
 resource "aws_iam_role" "ECSTaskExecutionRole" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
