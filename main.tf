94b2b07e2fd11a2f79bb4923c88160b44e495676
Brian Lutz <brian.lutz9@icloud.com>
2025-04-12 14:52:41 -0400

    Add `aws_alb_security_group` in main.tf

--- main.tf
+++ main.tf
@@ -81,7 +81,33 @@ resource "aws_security_group" "btlutz" {
     protocol    = -1
     self        = "false"
     cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.aws_alb_security_group.id]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "aws_alb_security_group" {
+  ingress {
+    description= "entry port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description= "django port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -93,7 +119,7 @@ resource "aws_security_group" "btlutz" {
 resource "aws_alb" "btlutz" {
   name               = "btlutz"
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.btlutz.id]
+  security_groups    = [aws_security_group.aws_alb_security_group.id]
   subnets            = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
 
   tags = {
