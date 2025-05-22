60c22f61e6d25a0c923bf3137a083cb14080fd15
Brian Lutz <brian.lutz9@icloud.com>
2025-05-21 22:56:37 -0400

    Change protocol to HTTPS in main.tf

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_lb_target_group" "btlutz" {
 resource "aws_lb_listener" "ecs_alb_listener" {
   load_balancer_arn = aws_alb.btlutz.arn
   port              = 80
-  protocol          = "HTTP"
+  protocol          = "HTTPS"
 
   default_action {
     type             = "forward"
