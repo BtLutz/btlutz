ed9483043a2839ffc2536ad3c739de0c31338dc9
Brian Lutz <brian.lutz9@icloud.com>
2025-05-21 23:51:56 -0400

    Delete the second route53 record in main.tf

--- main.tf
+++ main.tf
@@ -215,20 +215,3 @@ resource "aws_acm_certificate" "https" {
   domain_name       = "btlutz.com"
   validation_method = "EMAIL"
 }
-
-resource "aws_route53_record" "https" {
-  for_each = {
-    for dvo in aws_acm_certificate.https.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.primary.zone_id
-}
