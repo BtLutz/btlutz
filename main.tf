56d74fd63da58c0fc13924f29fc048fe0b995baa
Brian Lutz <brian.lutz9@icloud.com>
2025-06-09 22:33:38 -0400

    Replace from_port and to_port 80 with 443 in main.tf

--- main.tf
+++ main.tf
@@ -90,8 +90,8 @@ resource "aws_security_group" "btlutz" {
 resource "aws_security_group" "aws_alb_security_group" {
   ingress {
     description = "entry port"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
